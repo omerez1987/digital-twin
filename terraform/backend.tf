1a8a136bd7fd0f4fd9d83dc6d63bd531e0e6ab92
Ariel Omerez <omerez1987@yahoo.com>
2026-06-08 23:51:25 -0500

    fix for the error Error: Failed to get existing │Error: Argument or block definition required │ │   on backend.tf line 1: │    1: { │ │ An argument or block definition is required here.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,7 @@
-{
-  "bucket": "s3_frontend_bucket",
-  "key": "state/terraform.tfstate",
-  "region": "us-east-2"
+terraform {
+  backend "s3" {
+    bucket = "s3_frontend_bucket"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
