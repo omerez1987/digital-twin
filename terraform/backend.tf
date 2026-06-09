06ec3209c96e6dcd05b2f87bbf48d1aefdfab588
Ariel Omerez <omerez1987@yahoo.com>
2026-06-09 00:29:38 -0500

    fix for the error Error: Failed to get existing n backend.tf line 2, in terraform: │ Error: Failed to get existing workspaces: S3 bucket "twin-terraform-state-***" does not exist. │    2:   backend "s3" { │ │ │ The referenced S3 bucket must have been previously created. If the S3 bucket │ The parameter "dynamodb_table" is deprecated. Use parameter "use_lockfile" │ was created within the last minute, please wait for a minute or two and try │ instead. │ again. │ │ Error: operation error S3: ListObjectsV2, https response error StatusCode: 404, RequestID: 2A6W3N7KJRW279AR, HostID: TJDjxTqUnIScpkvrO2CvcvbI+GZjsqPOSy/lqczpM4KOa087uextY0C7UKBbRf6yMsQfJA1trUXM/UUWnkxBRnZho6u9A7Ai, NoSuchBucket: The specified bucket does not exist

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "s3-frontend-bucket-3"
+    bucket       = "twin-terraform-state-681885730381"
     key          = "state/terraform.tfstate"
     region       = "us-east-2"   # Set this to the AWS region where you created the bucket
     use_lockfile = true          # Fixes the 'dynamodb_table' deprecation warning
