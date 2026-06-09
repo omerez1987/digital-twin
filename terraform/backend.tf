4ff29258d215323e83a8591895e6f83b1d8c8fe6
Ariel Omerez <omerez1987@yahoo.com>
2026-06-08 23:42:02 -0500

    fix for the error Error: Failed to get existing workspaces: S3 bucket "twin-terraform-state-***" does not exist.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,4 @@
-terraform {
-  backend "s3" {
-    # These values will be set by deployment scripts
-    # For local development, they can be passed via -backend-config
-  }
-}
+terraform init \
+  -backend-config="bucket=s3_frontend_bucket" \
+  -backend-config="key=twin/terraform/terraform.tfstate" \
+  -backend-config="region=us-east-2"
