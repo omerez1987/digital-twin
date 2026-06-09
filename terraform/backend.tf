35c94e5501fe67044fb6771ce43d835892837b1b
Ariel Omerez <omerez1987@yahoo.com>
2026-06-08 23:47:43 -0500

    fix for the error Error: Failed to get existing │   on backend.tf line 1: │    1: terraform init \ │ │ Either a quoted string block label or an opening brace ("{") is expected │ here.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,5 @@
-terraform init \
-  -backend-config="bucket=s3_frontend_bucket" \
-  -backend-config="key=twin/terraform/terraform.tfstate" \
-  -backend-config="region=us-east-2"
+{
+  "bucket": "s3_frontend_bucket",
+  "key": "state/terraform.tfstate",
+  "region": "us-east-2"
+}
